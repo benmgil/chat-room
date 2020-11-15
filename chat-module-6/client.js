96f01c192189249e729e8a490588bb3f1ee31747
Zoe Levin <zoelevin@zoes-mbp-2.dhcp.wustl.edu>
2020-11-14 22:11:14 -0600

    client side stuff

--- chat-module-6/client.js
+++ chat-module-6/client.js
@@ -5,27 +5,39 @@ let createScreen;
 let browseScreen;
 let chatScreen;
 
-let loginButton;    //login
-let loginInput;     //username
-let roomInput;      //room-name
-let joinRoomButton;  //join-room
-let browseRoomsButton;   //browse
-let createRoomButton;    //create-room
-let passwordInput;      //password-input
+let errorMessage;
+let loginButton;    
+let loginInput;    
+let roomInput;     
+let joinRoomButton; 
+let joinPrivateRoomButton;
+let browseRoomsButton;   
+let createRoomButton;    
+let passwordInput;    
 let createNameInput;
 let createPasswordInput;
 let createButton;
+let roomList;
+
+let socket;
+let username;
+let roomName;
+let password;
 
 document.addEventListener("DOMContentLoaded", function(){
   loginScreen = document.getElementById("login-screen");
   homeScreen = document.getElementById("home-screen");
   passwordScreen = document.getElementById("password-screen");
   createScreen = document.getElementById("create-screen");
+  browseScreen = document.getElementById("browse-screen");
+  chatScreen = document.getElementById("chat-screen");
 
+  errorMessage = document.getElementById("error-message");
   loginButton = document.getElementById("login");
   loginInput = document.getElementById("username");
   roomInput = document.getElementById("room-name");
   joinRoomButton = document.getElementById("join-room");
+  joinPrivateRoomButton = document.getElementById("join-private-room");
   browseRoomsButton = document.getElementById("browse");
   createRoomButton = document.getElementById("create-room");
   passwordInput = document.getElementById("password-input");
@@ -33,6 +45,131 @@ document.addEventListener("DOMContentLoaded", function(){
   createPasswordInput = document.getElementById("new-room-password");
   createButton = document.getElementById("create-room-button");
   browseScreen = document.getElementById("browse-screen");
+  roomList = document.getElementById("room-list");
 
-
+  loginButton.addEventListener("click", signOn);
+  joinRoomButton.addEventListener("click", joinRoom)
+  browseRoomsButton.addEventListener("click", toRoomsList)
+  createRoomButton.addEventListener("click", toCreateRoom);
+  createButton.addEventListener("click", createRoom)
+  joinPrivateRoomButton.addEventListener("click", joinPrivateRoom);
 })
+
+//logging in
+function signOn(){
+  if(loginInput.value == ""){
+    errorMessage.innerText = "Error: Please enter a username."
+  }
+  else{
+    username = loginInput.value;
+    loginScreen.style.display="none";
+    homeScreen.style.display="block";
+    errorMessage.innerText = "";
+    socket = io.connect();
+    socket.on("request_username", function(){
+      socket.emit("login", {username:username});
+    })
+  }
+}
+
+//joining room
+function joinRoom(){
+  if(roomInput.value == ""){
+    errorMessage.innerText = "Error: Please enter a room name."
+  }
+  else{
+    roomName = roomInput.value;
+    errorMessage.innerText = "";
+    socket.emit("join_room", {username:username, roomName:roomName});
+  }
+  socket.on("join_response", function(data){
+    if(data.status == "success"){
+      homeScreen.style.display = "none";
+      chatScreen.style.disoplay = "block";
+    }  
+    else if(data.status == "password_required"){
+      homeScreen.style.display = "none";
+      passwordScreen.style.display = "block";
+    }
+    else{
+      errorMessage.innerText= data.message;
+    }
+  })
+}
+
+//joining private room
+function joinPrivateRoom(){
+  if(passwordInput.value == ""){
+    errorMessage.innerText = "Error: Please enter a password."
+  }
+  else{
+    password = passwordInput.value;
+    socket.emit("password_entered", {password:password});
+    socket.on("password_response", function(data){
+      if(data.status == "success"){
+        errorMessage.innerText = "";
+        passwordScreen.style.display = "none";
+        chatScreen.style.display = "block";
+      }
+      else{
+        errorMessage.innerText = data.message;
+      }
+    });
+  }
+}
+
+//choosing to create a room
+function toCreateRoom(){
+  homeScreen.style.display="none";
+  createScreen.style.display="block";
+}
+
+//creating a new room 
+function createRoom(){
+  //validating input existence and storing input values
+  if(createNameInput.value == ""){
+    errorMessage.innerText = "Error: Please enter a room name."
+  }
+  else{
+    roomName = createNameInput.value;
+  }
+  if( !(createPasswordInput.value == "") ){
+    password = createPasswordInput.value;
+  }
+
+  //send info to server
+  socket.emit("create_room", { roomName:roomName, password:password});
+
+  socket.on("create_response", function(data){
+    if(data.status == "success"){
+      createScreen.style.display="none";
+      chatScreen.style.display="block";
+      errorMessage.innerText = "";
+    }
+    else{
+      errorMessage.innerText = data.message;
+    }
+  })
+
+}
+
+//to browse current rooms
+function toRoomsList(){
+  homeScreen.style.display="none";
+  browseScreen.style.display="block";
+  roomList.innerHTML = "";
+
+  socket.emit("showShit");
+  //socket.emit("request_rooms_list");
+  socket.on("room_list_response", function(data){ //for each received room, create and add room to room list div
+    data.roomList.forEach(function(room, i){
+      let roomp = document.createElement("p");
+      roomp.className = "room-list";
+      roomp.innerText = data.roomList.roomName;
+      if(data.roomList.isLocked){
+        roomp.innerText += " (locked)";
+      }
+      roomList.appendChild(roomp);
+    })
+  });
+}
